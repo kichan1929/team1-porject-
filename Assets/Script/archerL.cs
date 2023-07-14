f97b02066d05ab62eff58774254b04e4ddecbfa0
gana0201 <155277541gaeun@gmail.com>
2023-07-14 23:08:58 +0900

    궁수 프로젝트 파일

---
+++ Assets/Script/archerL.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class archerL : MonoBehaviour
+{
+    public float speed = 1;
+    int bow = 1; // 임의 값 지정
+    int count;
+
+    void Start()
+    {
+        count = 0;
+    }
+    void FixedUpdate()
+    {
+        if (count <= 100)
+        {
+            if (bow == 1) 
+            {
+                this.transform.Translate(speed / -20, 0, 0);
+                count = count + 1;
+            }
+        }
+
+    }
+
+}
